35316d208d0b29c2c10cc03f5175b85966a769e6
issac-326 <issacjamaelc00@gmail.com>
2024-10-12 23:16:53 -0600

    aa

--- src/components/MenuCategorias.tsx
+++ src/components/MenuCategorias.tsx
@@ -4,9 +4,9 @@ import React, { useState, useEffect } from 'react';
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
-  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import {faMagnifyingGlass, faChevronDown, faSliders, faGear, faChevronRight} from '@fortawesome/free-solid-svg-icons'
-  import {faBell, faUser, faHeart, faCommentDots, } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faMagnifyingGlass, faChevronDown,faChevronRight} from '@fortawesome/free-solid-svg-icons'
+import {faBell } from '@fortawesome/free-regular-svg-icons'
 import { getCategorias, getCategoriaEspecifica } from "@/app/menu/home/actions";  
 
 interface Categoria {
