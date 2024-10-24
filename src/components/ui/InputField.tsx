0506b1b80c53aa8ebd9cc49c67c63213f3f16b91
DoubtfulCafe <dvilleda2007@gmail.com>
2024-10-24 02:10:07 -0600

    validaciones y mensajes de error

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 
+
 interface InputProps {
   id: string;
   name: string;
@@ -9,9 +10,9 @@ interface InputProps {
   value: string;
   placeholder: string;
   required?: boolean;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;  // Hacer onChange opcional
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // Hacer onChange opcional
   minLength?: number;
-  readOnly?: boolean;  // Añadimos readOnly como una propiedad opcional
+  readOnly?: boolean;// Añadimos readOnly como una propiedad opcional
 }
 
 const InputField: React.FC<InputProps> = ({
@@ -21,7 +22,7 @@ const InputField: React.FC<InputProps> = ({
   placeholder,
   value,
   onChange,
-  readOnly = false,  // Valor por defecto para readOnly
+  readOnly = false, // Valor por defecto para readOnly
 }) => {
   return (
       <input
@@ -30,11 +31,14 @@ const InputField: React.FC<InputProps> = ({
         type={type}
         placeholder={placeholder}
         value={value}
-        onChange={onChange}  // Esto será opcional
-        readOnly={readOnly}  // Aplicamos readOnly
-        className="focus:outline-none focus:border-blue-800/2 placeholder:text-gray-400 text-xs text-black bg-white rounded-[50px] pl-5 pt-2 mb-5 w-[330px] h-[35px] shadow-[0_4px_4px_rgba(0,0,0,0.25)]"
+        onChange={onChange}// Esto será opcional
+        readOnly={readOnly}// Aplicamos readOnly
+        className={`focus:outline-none focus:border-blue-800/2 placeholder:text-gray-400 text-xs text-black rounded-[50px] pl-5 pt-2 mb-5 w-[330px] h-[35px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] ${
+          readOnly ? 'bg-gray-200 text-gray-500 cursor-not-allowed' : 'bg-white'
+        }`}
       />
   );
 };
+
 
 export default InputField;
