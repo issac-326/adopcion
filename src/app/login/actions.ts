a1f0cc830525cff7721f98d43a7ce4a525b7173f
issac-326 <126538451+issac-326@users.noreply.github.com>
2024-11-07 00:06:41 -0600

    Create actions.ts

---
+++ src/app/login/actions.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import bcrypt from 'bcrypt'; // Asegúrate de importar bcrypt
+import { createClient } from '@/utils/supabase/server';
+
+ 
+
+export const loginUser = async (formData: FormData) => { 
+  const supabase = createClient();
+
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  // Realiza una consulta a la tabla usuarios
+  const { data, error } = await supabase
+    .from('usuarios') // Asegúrate de que el nombre de la tabla sea correcto
+    .select('id_usuario, correo, contrasena') // Seleccionamos 'id_usuario', 'correo' y 'contrasena'
+    .eq('correo', email)
+    .single();
+
+  if (error || !data) {
+    throw new Error('Credenciales incorrectas'); // Manejar caso de credenciales incorrectas
+  }
+
+  // Compara la contraseña ingresada con la almacenada
+  const isPasswordValid = await bcrypt.compare(password, data.contrasena);
+  
+  if (!isPasswordValid) {
+    throw new Error('Credenciales incorrectas'); // La contraseña es incorrecta
+  }
+
+  return data; // Retornar los datos del usuario autenticado
+};
