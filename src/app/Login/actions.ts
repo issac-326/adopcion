fd7a3b3c7b6476f9f1b826ecc2fa9690c7d664fe
DoubtfulCafe <dvilleda2007@gmail.com>
2024-11-12 22:47:43 -0600

    y este merge?

--- src/app/Login/actions.ts
+++ src/app/Login/actions.ts
@@ -9,25 +9,25 @@ export const loginUser = async (formData: FormData) => {
   const supabase = createClient();
 
   const email = formData.get('email');
-  const password = formData.get('password');
+  const password = formData.get('password') as string;
 
   // Realiza una consulta a la tabla usuarios
   const { data, error } = await supabase
-    .from('usuarios') // Asegúrate de que el nombre de la tabla sea correcto
-    .select('id_usuario, correo, contrasena') // Seleccionamos 'id_usuario', 'correo' y 'contrasena'
+    .from('usuarios') 
+    .select('id_usuario, correo, contrasena') 
     .eq('correo', email)
     .single();
 
   if (error || !data) {
-    throw new Error('Credenciales incorrectas'); // Manejar caso de credenciales incorrectas
+    throw new Error('Credenciales incorrectas'); 
   }
 
   // Compara la contraseña ingresada con la almacenada
   const isPasswordValid = await bcrypt.compare(password, data.contrasena);
   
   if (!isPasswordValid) {
-    throw new Error('Credenciales incorrectas'); // La contraseña es incorrecta
+    throw new Error('Credenciales incorrectas'); 
   }
 // se borro_
-  return data; // Retornar los datos del usuario autenticado
+  return data; 
 };
