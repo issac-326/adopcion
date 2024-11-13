5feb359d2ef7a34e72f89ebfdcb19e79449684d9
DoubtfulCafe <dvilleda2007@gmail.com>
2024-11-12 23:42:40 -0600

    ...

---
+++ src/app/Login/actions.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import bcrypt from 'bcrypt'; // Asegúrate de importar bcrypt
+import { createClient } from '@/utils/supabase/server';
+
+ 
+
+export const loginUser = async (formData: FormData) => { 
+  const supabase = createClient();
+
+  const email = formData.get('email');
+  const password = formData.get('password') as string;
+
+  // Realiza una consulta a la tabla usuarios
+  const { data, error } = await supabase
+    .from('usuarios') 
+    .select('id_usuario, correo, contrasena') 
+    .eq('correo', email)
+    .single();
+
+  if (error || !data) {
+    throw new Error('Credenciales incorrectas'); 
+  }
+
+  // Compara la contraseña ingresada con la almacenada
+  const isPasswordValid = await bcrypt.compare(password, data.contrasena);
+  
+  if (!isPasswordValid) {
+    throw new Error('Credenciales incorrectas'); 
+  }
+// se borro_
+  return data; 
+};
