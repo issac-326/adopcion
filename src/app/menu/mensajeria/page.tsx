fe3de4a01646597f234f9133e4c15ab4136c7093
KennethJContreas <kennethcontreras2017@gmail.com>
2024-11-15 18:44:41 -0600

    arreglando fronted de msj

--- src/app/menu/mensajeria/page.tsx
+++ src/app/menu/mensajeria/page.tsx
@@ -15,6 +15,7 @@ const Chat = () => {
   const [activeConversationId, setActiveConversationId] = useState(null);
   const [receiverUID, setReceiverUID] = useState(null);
 
+  //trae el usuario emisor y conversaciones
   useEffect(() => {
     const fetchUser = async () => {
       try {
@@ -30,22 +31,7 @@ const Chat = () => {
     fetchConversations();
   }, []);
 
-  /*   useEffect(() => {
-      const fetchUser = async () => {
-        try {
-          const userReceptor = await getUserProfile(receiverUID);
-          setUserReceptor(userReceptor);
-          console.log("userReceptor", userReceptor);
-        } catch (error) {
-          console.error("Error al obtener el usuario:", error);
-        }
-      };
-  
-      fetchUser().then(() => {
-        fetchMessages();
-      });
-    }, [receiverUID]); */
-
+  //conexion con cometChat
   useEffect(() => {
     const initCometChat = async () => {
       const appID = process.env.NEXT_PUBLIC_COMETCHAT_APP_ID;
@@ -86,7 +72,7 @@ const Chat = () => {
     initCometChat();
   }, []);
 
-
+  //trae los mensajes del usuario receptor
   useEffect(() => {
     const obtenerUsuarioPorUID = (uid) => {
       return new Promise((resolve, reject) => {
@@ -189,11 +175,11 @@ const Chat = () => {
 
   return (
     userEmisor && (
-      <div className="flex min-h-screen w-full mt-5 bg-[#c7b69f]">
+      <div className="flex min-h-screen w-full bg-[#cceef0] rounded-xl">
 
         {/* Panel lateral de conversaciones */}
-        <div className="w-2/5 bg-gray-100 p-4 space-y-4 overflow-y-auto border-r bg-[#7e634e]">
-          <h2 className="text-xl font-semibold mb-4">Chats</h2>
+        <div className="w-2/5 bg-[#2f797d] py-4 px-2 space-y-0 overflow-y-auto border-r rounded-tl-xl rounded-bl-xl text-[#fdd5d5]">
+          <h2 className="text-xl font-semibold mb-4 text-white">Chats</h2>
           {conversations.map((conversation, idx) => {
             const receiver = conversation.conversationWith;
             const lastMessage = conversation.lastMessage;
@@ -210,7 +196,7 @@ const Chat = () => {
             return (
               <div
                 key={idx}
-                className={`p-3 rounded-lg cursor-pointer hover:bg-gray-200 ${conversation.conversationId === activeConversationId ? 'bg-blue-100' : 'bg-white'}`}
+                className={`p-3 rounded-lg cursor-pointer hover:bg-[#dbeafe3b] ${conversation.conversationId === activeConversationId ? 'bg-blue-100' : ''}`}
                 onClick={() => { setActiveConversationId(conversation.conversationId); setReceiverUID(conversation.receiver); }}
               >
                 <div className="flex items-center space-x-2">
@@ -221,7 +207,7 @@ const Chat = () => {
                   />
                   <div>
                     <p className="font-semibold">{receiverName}</p>
-                    <p className="text-xs text-gray-500">{lastMessage}</p>
+                    <p className="text-xs text-[#aed3df]">{lastMessage}</p>
                     <p className="text-xs text-gray-400">{formatTime(sentAt)}</p>
                   </div>
                 </div>
@@ -232,102 +218,103 @@ const Chat = () => {
         </div>
 
         {/* Panel de mensajes */}
-        {messages.length === 0 ? (
-          <div className="w-full flex-grow flex flex-col items-center justify-center bg-gray-50 p-6 space-y-4">
-            <img src="\img\logos\mensajeria.png" alt="Logo" className="max-w-[70%] object-contain" />
-            <p className="text-center text-lg text-gray-700 font-medium max-w-[70%]">
-              Comienza a conversar y a conectarte con otras personas para adoptar a tu compañero de vida
-            </p>
-          </div>) :
-          (<div className="flex flex-col flex-1 h-full w-full">
+        {messages.length === 0 ?
+          (
+            <div className="w-full flex-grow flex flex-col items-center justify-center p-6 space-y-4">
+              <img src="\img\logos\mensajeria.png" alt="Logo" className="max-w-[70%] object-contain" />
+              <p className="text-center text-lg text-gray-700 font-medium max-w-[70%]">
+                Comienza a conversar y a conectarte con otras personas para adoptar a tu compañero de vida
+              </p>
+            </div>) :
+          (
+            <div className="flex flex-col flex-1 h-screen w-full">
 
-            {/* Título */}
-            <div className="relative flex gap-2 items-center bg-blue-500 w-full text-white text-center px-4 py-2">
-              <img src="/usuario-default.png" alt="avatar" className="rounded-full w-9" />
-              <div className="flex flex-col items-start">
-                <p>{userReceptor.name}</p>
-                <p className="text-xs">{userReceptor.status !== 'offline' ? 'en linea' : `ultima vez a las ${new Date(userReceptor.lastActiveAt * 1000).toLocaleTimeString([], {
-                  hour: '2-digit',
-                  minute: '2-digit',
-                })}`}</p>
+              {/* Título */}
+              <div className="relative max-h-full flex gap-2 items-center bg-blue-500 w-full text-white text-center px-4 py-2">
+                <img src="/usuario-default.png" alt="avatar" className="rounded-full w-9" />
+                <div className="flex flex-col items-start">
+                  <p>{userReceptor.name}</p>
+                  <p className="text-xs">{userReceptor.status !== 'offline' ? 'en linea' : `ultima vez a las ${new Date(userReceptor.lastActiveAt * 1000).toLocaleTimeString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  })}`}</p>
+                </div>
               </div>
-            </div>
 
-            {/* Mensajes */}
-            {messages.length === 0 ? (
-              <div className="flex-1 overflow-y-auto p-4 space-y-4">
-                {[...Array(5)].map((_, idx) => (
-                  <div key={idx} className="flex justify-start items-start space-x-2 animate-pulse">
-                    <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
-                    <div className="ml-0 p-3 rounded-lg bg-gray-300 max-w-[60%]">
-                      <div className="block mb-1 text-sm bg-gray-400 w-24 h-4 rounded-md"></div>
-                      <div className="w-36 h-4 bg-gray-400 rounded-md"></div>
-                      <div className="mt-2 w-16 h-3 bg-gray-400 rounded-md text-right"></div>
-                    </div>
-                  </div>
-                ))}
-                <div ref={messagesEndRef} />
-              </div>
-            ) : (
-              <div className="flex-1 overflow-y-auto p-4 space-y-4">
-                {messages.map((msg, idx) => {
-                  const isSender = msg.sender.uid == userEmisor.id_usuario;
-                  const previousMessage = messages[idx - 1];
-                  const showAvatar = !previousMessage || previousMessage.sender.uid !== msg.sender.uid;
-
-                  return (
-                    <div key={idx} className={`flex ${isSender ? 'justify-end' : 'justify-start'} items-start space-x-2`}>
-                      {!isSender && showAvatar && (
-                        <img
-                          src={userReceptor ? userReceptor.imagen : '/default-avatar.png'}
-                          alt="Avatar"
-                          className="w-10 h-10 rounded-full"
-                        />
-                      )}
-                      <div
-                        className={`p-3 rounded-lg ml-${showAvatar ? '0' : '12'} text-white max-w-[60%] ${isSender
-                          ? 'bg-gradient-to-r from-[#acd094] to-[#f5a473]'
-                          : 'bg-gradient-to-r from-[#7d86a5] to-[#f39893]'
-                          }`}
-                      >
-                        {!isSender && showAvatar && <span className="block mb-1 text-sm font-semibold">{msg.sender.name}</span>}
-                        <span>{msg.text}</span>
-                        <div className="text-xs text-gray-200 mt-2 text-right">
-                          {new Date(msg.sentAt * 1000).toLocaleTimeString([], {
-                            hour: '2-digit',
-                            minute: '2-digit',
-                          })}
-                        </div>
+              {/* Mensajes */}
+              {messages.length === 0 ? (
+                <div className="flex-1 overflow-y-auto p-4 space-y-4">
+                  {[...Array(5)].map((_, idx) => (
+                    <div key={idx} className="flex justify-start items-start space-x-2 animate-pulse">
+                      <div className="w-10 h-10 bg-gray-300 rounded-full"></div>
+                      <div className="ml-0 p-3 rounded-lg bg-gray-300 max-w-[60%]">
+                        <div className="block mb-1 text-sm bg-gray-400 w-24 h-4 rounded-md"></div>
+                        <div className="w-36 h-4 bg-gray-400 rounded-md"></div>
+                        <div className="mt-2 w-16 h-3 bg-gray-400 rounded-md text-right"></div>
                       </div>
-                      {isSender && showAvatar && (
-                        <img
-                          src={userEmisor ? userEmisor.imagen : '/default-avatar.png'}
-                          alt="Avatar"
-                          className="w-10 h-10 rounded-full"
-                        />
-                      )}
                     </div>
-                  );
-                })}
-                <div ref={messagesEndRef} />
+                  ))}
+                  <div ref={messagesEndRef} />
+                </div>
+              ) : (
+                <div className="flex-1 overflow-y-auto p-4 space-y-4">
+                  {messages.map((msg, idx) => {
+                    const isSender = msg.sender.uid == userEmisor.id_usuario;
+                    const previousMessage = messages[idx - 1];
+                    const showAvatar = !previousMessage || previousMessage.sender.uid !== msg.sender.uid;
+
+                    return (
+                      <div key={idx} className={`flex ${isSender ? 'justify-end' : 'justify-start'} items-start space-x-2`}>
+                        {!isSender && showAvatar && (
+                          <img
+                            src={userReceptor ? userReceptor.imagen : '/default-avatar.png'}
+                            alt="Avatar"
+                            className="w-10 h-10 rounded-full"
+                          />
+                        )}
+                        <div
+                          className={`p-3 rounded-lg ml-${showAvatar ? '0' : '12'} text-white max-w-[60%] ${isSender
+                            ? 'bg-[#7E634E]'
+                            : 'bg-[#C7B69F]'
+                            }`}
+                        >
+                          {!isSender && showAvatar && <span className="block mb-1 text-sm font-semibold">{msg.sender.name}</span>}
+                          <span>{msg.text}</span>
+                          <div className="text-xs text-gray-200 mt-2 text-right">
+                            {new Date(msg.sentAt * 1000).toLocaleTimeString([], {
+                              hour: '2-digit',
+                              minute: '2-digit',
+                            })}
+                          </div>
+                        </div>
+                        {isSender && showAvatar && (
+                          <img
+                            src={userEmisor ? userEmisor.imagen : '/default-avatar.png'}
+                            alt="Avatar"
+                            className="w-10 h-10 rounded-full"
+                          />
+                        )}
+                      </div>
+                    );
+                  })}
+                  <div ref={messagesEndRef} />
+                </div>
+              )}
+
+              {/* Input y botón para enviar mensaje */}
+              <div className="grid grid-cols-6 pt-2 bg-white border-t gap-1">
+                <input
+                  className="flex-1 border border-gray-300 p-2 rounded-lg focus:outline-none focus:border-blue-500 col-span-5"
+                  type="text"
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  placeholder="Escribe tu mensaje..."
+                />
+                <button className="w-[2] h-[40px] bg-[#FE8A5B] grid-cols-1 rounded-sm text-sm text-white hover:bg-[#ff9060]" onClick={sendMessage}>
+                  Enviar
+                </button>
               </div>
-            )}
-
-            {/* Input y botón para enviar mensaje */}
-            <div className="grid grid-cols-6 pt-2 bg-white border-t gap-1">
-              <input
-                className="flex-1 border border-gray-300 p-2 rounded-lg focus:outline-none focus:border-blue-500 col-span-5"
-                type="text"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="Escribe tu mensaje..."
-              />
-              <button className="w-[2] h-[40px] bg-[#FE8A5B] grid-cols-1 rounded-sm text-sm text-white hover:bg-[#ff9060]" onClick={sendMessage}>
-                Enviar
-              </button>
-            </div>
-          </div>)}
-
+            </div>)}
       </div>
     )
   );
