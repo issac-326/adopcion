4c2269d4337a618e366ab6293ee83bbe9074855a
KennethJContreas <kennethcontreras2017@gmail.com>
2024-11-17 22:28:19 -0600

    real time

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -3,16 +3,18 @@ import Header from '@/components/Header';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex min-h-full">
+    <div className="flex h-screen">
       <Header />
-      <main className="flex-1 overflow-auto w-full h-full">
-        <div className="relative bg-white border mt-2 mb-4 mx-4 min-h-[full] h-[90%] rounded-xl shadow-[0_3px_6px_rgba(0,0,0,0.15),0_8px_16px_rgba(0,0,0,0.1),0_20px_30px_rgba(0,0,0,0.05)]">
+      <main className="flex-1 overflow-auto w-full ">
+        <div className=" relative bg-white border mt-2 mb-4  mx-4 min-h-screen rounded-xl shadow-[0_3px_6px_rgba(0,0,0,0.15),0_8px_16px_rgba(0,0,0,0.1),0_20px_30px_rgba(0,0,0,0.05)]
+">
+
           {children}
+
         </div>
       </main>
     </div>
-
   );
 };
-
+ 
 export default Layout;
