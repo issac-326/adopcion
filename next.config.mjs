f109e68170b16f9b1470e31bb7e7fe0c061b1f2e
issac-326 <issacjamaelc00@gmail.com>
2024-11-18 20:46:33 -0600

    detalles eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        domains: ['res.cloudinary.com'],
-      },
-
+  reactStrictMode: false, // Puedes dejarlo en false si no necesitas reactStrictMode por ahora
+  images: {
+    domains: ['res.cloudinary.com'], // Configuración para tus imágenes
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Deshabilita ESLint durante la construcción
+  },
 };
 
 export default nextConfig;
+
