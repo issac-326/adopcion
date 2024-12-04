545a5033504c23b7ac7f51e5f11a2ccfc1192ec4
DoubtfulCafe <dvilleda2007@gmail.com>
2024-12-04 12:01:48 -0600

    implementacion de logica para que aparezca el botones para administradores y botones para moderadores

--- src/app/administrator/layout.tsx
+++ src/app/administrator/layout.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/HeaderAdmin';
 import { validateGeneralAccess } from './actions';
 import { redirect } from 'next/navigation';
 import { faUser, faHeart, faCommentDots } from '@fortawesome/free-regular-svg-icons';
-import { faBug, faGear, faPlus, faSignOutAlt, faUserShield, faListCheck } from '@fortawesome/free-solid-svg-icons';
+import { faBug, faGear, faPlus, faSignOutAlt, faUserShield, faListCheck,faUsersRectangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
@@ -33,9 +33,22 @@ export default async function AdministratorLayout({ children }: { children: Reac
     { icon: <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M180-475q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Zm180-160q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Zm240 0q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29Zm180 160q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM266-75q-45 0-75.5-34.5T160-191q0-52 35.5-91t70.5-77q29-31 50-67.5t50-68.5q22-26 51-43t63-17q34 0 63 16t51 42q28 32 49.5 69t50.5 69q35 38 70.5 77t35.5 91q0 47-30.5 81.5T694-75q-54 0-107-9t-107-9q-54 0-107 9t-107 9Z" /></svg>, label: "Inicio", slug: "" },
     { icon: <FontAwesomeIcon icon={faListCheck as IconProp} className="w-5 h-5" />, label: "Validar Mascotas" , slug: "/validaciones" },
     { icon: <FontAwesomeIcon icon={faUserShield as IconProp} className='w-5 h-5'/>, label: "Reportes de usuarios", slug: "/reporte-usuarios" },
-    { icon: <FontAwesomeIcon icon={faUser as IconProp} className="w-5 h-5" />, label: "Gestion de moderadores" , slug: "/moderacion"},
+    ...(access.role === 'admin'
+      ? [{
+          icon: <FontAwesomeIcon icon={faUser as IconProp} className="w-5 h-5" />, 
+          label: "Gestión de moderadores", 
+          slug: "/moderacion"
+        },
+        {
+          icon: <FontAwesomeIcon icon={faUsersRectangle as IconProp} className="w-5 h-5" />, 
+          label: "Gestión de moderadores", 
+          slug: "/moderacion/moderators"
+        },
+       ]
+      : []
+    ),
     { icon: <FontAwesomeIcon icon={faBug as IconProp} className="w-5 h-5" />, label: "Log de soporte" , slug: "/reporteSoporte"},
-    { icon: <FontAwesomeIcon icon={faGear as IconProp} className="w-5 h-5" />, label: "Configuraciones" , slug: "/configuraciones"},
+   
   ];
 /*
 validar una mascota 
