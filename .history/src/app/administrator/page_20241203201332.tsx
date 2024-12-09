11d4f6d93ff6192ff0fb8d68760948b49346bce3
issac-326 <issacjamaelc00@gmail.com>
2024-12-08 18:38:18 -0600

    borrar history

--- .history/src/app/administrator/page_20241203201332.tsx
+++
@@ -1,9 +0,0 @@
-export default function AdministratorDashboard() {
-    return (
-      <div>
-        <h2>Bienvenido al Panel de Administración</h2>
-        <p>Aquí puedes gestionar los recursos del sistema.</p>
-      </div>
-    );
-  }
-  
